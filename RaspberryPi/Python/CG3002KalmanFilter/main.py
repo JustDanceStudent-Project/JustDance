725bd2c380ec4bbc49763da55b1d94b067877d38
Yip Bing You <leafytubistfriend@gmail.com>
2017-09-20 06:49:14 +0800

    Initial KalmanFilter lib import
    Using anaconda + spyder + filterpy

---
+++ RaspberryPi/Python/CG3002KalmanFilter/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 10:39:59 2017
+
+@author: avidr
+"""
+import numpy as np
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+
+
